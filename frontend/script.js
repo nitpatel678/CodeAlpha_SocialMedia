2dc0411aa3a47e0a5eca97753e0e0de5ca14536d
Nitin Patel <151562820+nitpatel678@users.noreply.github.com>
2025-07-29 00:19:04 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://codealpha-socialmedia-x1if.onrender.com";
 let currentUser = null;
 let currentPostType = "text";
 
@@ -210,7 +210,7 @@ function createPostElement(post, isLiked) {
                     <p>${post.content}</p>
                     ${
                       post.image
-                        ? `<img src="http://localhost:5000${post.image}" alt="Post image" class="post-image">`
+                        ? `<img src="${API_BASE}${post.image}" alt="Post image" class="post-image">`
                         : ""
                     }
                 </div>
